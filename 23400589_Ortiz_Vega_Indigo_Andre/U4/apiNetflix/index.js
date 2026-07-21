552a367d7f0007c7720674a6fc7db7e3969a706a
Índigo Andre Ortiz Vega <inanortizve@ittepic.edu.mx>
2026-07-21 09:31:43 -0700

    apiNetflix

--- 23400589_Ortiz_Vega_Indigo_Andre/U4/apiNetflix/index.js
+++ 23400589_Ortiz_Vega_Indigo_Andre/U4/apiNetflix/index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
